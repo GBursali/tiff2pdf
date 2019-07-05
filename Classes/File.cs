85263166c529a639f6ded277170b9b056e868f77
Gurkan BURSALI <38786334+GBursali@users.noreply.github.com>
2019-07-05 20:10:13 +0300

    Add files via upload

---
+++ Classes/File.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.Win32;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using Tiff2Pdf.Properties;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.Office.Interop.Word;
+using Task = System.Threading.Tasks.Task;
+#pragma warning disable 1998
+
+namespace Tiff2Pdf.Classes
+{ 
+
+    /// <summary>
+    /// Window States
+    /// </summary
+    public enum Stat
+    {
+        READY,CONVERTED
+    }
+
+    /// <summary>
+    /// File class for converting.
+    /// </summary>
+    public sealed class ConvertFile : INotifyPropertyChanged
+    {
+        #region Privates
+
+        private string _filename = string.Empty;
+        private Stat _status = Stat.READY;
+        private double _value = 0.0;
+
+        #endregion
+
+        #region INotfiy
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Notify([CallerMemberName]string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        #endregion
+
+        /// <summary>
+        /// FullName of File
+        /// </summary>
+        public string FileName
+        {
+            get { return _filename; }
+            set
+            {
+                if (value == _filename)
+                    return;
+
+                _filename = value;
+                Notify();
+            }
+        }
+
+        /// <summary>
+        /// Percentage
+        /// </summary>
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value == _value)
+                    return;
+
+                _value = value;
+                Notify();
+            }
+        }
+
+        /// <summary>
+        /// File Status
+        /// </summary>
+        public Stat Status
+        {
+            get { return _status; }
+            private set
+            {
+                _status = value;
+                Notify();
+                //Notify("color");
+            }
+        }
+
+        /// <summary>
+        /// File's name
+        /// </summary>
+        public string FileShort => global::System.IO.Path.GetFileName(FileName);
+
+        /// <summary>
+        /// TIFF to PDF
+        /// </summary>
+        public async void Convert()
+        {
+            try
+            {
+                using (var pd = new PdfDocument())
+                {
+                    var bpm = Image.FromFile(FileName);
+                    using (bpm)
+                    {
+                        for (var i = 0; i < bpm.GetFrameCount(FrameDimension.Page); i++)
+                            using (var byteStream = new MemoryStream())
+                            {
+                                bpm.SelectActiveFrame(FrameDimension.Page, i);
+                                bpm.Save(byteStream, ImageFormat.Jpeg);
+                                var ppage = pd.AddPage();
+                                using (var gfx = XGraphics.FromPdfPage(ppage))
+                                {
+                                    gfx.DrawImage(XImage.FromStream(byteStream), 0, 0);
+                                }
+                            }
+                    }
+
+                    pd.Save(Path());
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception Occured:" + e.Message);
+                return;
+            }
+
+            Status = Stat.CONVERTED;
+            CheckPermissionAndFire(Settings.Default.openAfterCon,
+                                   () => { global::System.Diagnostics.Process.Start(Path()); });
+            Notify("fileShort");
+        }
+
+        /// <summary>
+        /// Gives the Path for this file according to settings.
+        /// </summary>
+        /// <returns>Path</returns>
+        private string Path()
+        {
+            var path = "";
+            CheckPermissionAndFire(Settings.Default.useSamePath, () => { path = global::System.IO.Path.Combine(global::System.IO.Path.GetFullPath(FileName).Substring(0, FileName.LastIndexOf(".", StringComparison.Ordinal)) + ".pdf"); });
+
+            if (!string.IsNullOrEmpty(path))
+                return path;
+
+            var sfd = new SaveFileDialog
+            {
+                Title = "Choose a Path to save",
+                CheckPathExists = true,
+                AddExtension = false,
+                Filter = "PDF Files|*.pdf",
+                FileName = global::System.IO.Path.GetFileNameWithoutExtension(FileName) + ".pdf",
+                OverwritePrompt = true
+            };
+
+            if (sfd.ShowDialog() == true)
+                path= sfd.FileName;
+            else
+                MessageBox.Show("You have to select a Path");
+
+            return path;
+        }
+
+        /// <summary>
+        /// If you have the 'perm'ission, 'Fire'
+        /// </summary>
+        /// <param name="perm">Permission</param>
+        /// <param name="fire">Event</param>
+        public static void CheckPermissionAndFire(bool perm, Action fire)
+        {
+            if (perm) fire();
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Tiff2Pdf.Classes;
+using Tiff2Pdf.Properties;
+
+namespace Tiff2Pdf
+{
+    public partial class MainWindow:INotifyPropertyChanged
+    {
+
+        #region Notify Stuff
+
+        /// <summary>
+        /// NotifyPropertyChanged Implement
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifier
+        /// </summary>
+        /// <param name="prop">Notified Property Name</param>
+        private void Notify([CallerMemberName]string prop="")=>PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        #endregion
+
+        #region Private Stuff
+
+        /// <summary>
+        /// State for Iconize. true if iconized
+        /// </summary>
+        private bool _state = true;
+
+        /// <summary>
+        /// File List
+        /// </summary>
+        private ObservableCollection<ConvertFile> _fileList = new ObservableCollection<ConvertFile>();
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Window's State. Converted to Boolean in the XAML Window.
+        /// </summary>
+        public bool State { get => _state;
+            set { _state = value;Notify(); } }
+
+        /// <summary>
+        /// Convert List's all files
+        /// </summary>
+        public ObservableCollection<ConvertFile> FileList { get => _fileList;
+            set { _fileList = value; Notify(); } }
+
+        /// <summary>
+        /// Ekranda ortada yazılacak metin.
+        /// </summary>
+        public string MiddleText => string.Format("{0} Files are Ready", FileList.Count);
+
+        #endregion
+
+        /// <inheritdoc />
+        /// <summary>
+        /// START:Initialize
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileList.CollectionChanged += CheckAndIfConvert;
+            Ni.Click += (s, e) => { Iconize(null, null); };
+            Ni.Icon = new System.Drawing.Icon("Tiff2Pdf.ico");
+        }
+
+        #region IconizingStuff
+
+        /// <summary>
+        /// Program's Icon
+        /// </summary>
+        public System.Windows.Forms.NotifyIcon Ni = new System.Windows.Forms.NotifyIcon()
+        {
+            BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning,
+            BalloonTipText = @"Waits Here",
+            BalloonTipTitle = @"Tiff2Pdf",
+            Visible = true
+        };
+
+        /// <summary>
+        /// Iconize Click
+        /// </summary>
+        /// <param name="sender">_ button</param>
+        /// <param name="e">Button.Click</param>
+        private void Iconize(object sender, RoutedEventArgs e)
+        {
+            State = !State;
+            Ni.BalloonTipText = @"Program " + ((State) ? "is on the screen" : "is hidden");
+            Ni.ShowBalloonTip(1000);
+        }
+
+        #endregion
+
+        #region Adding to List
+
+        /// <summary>
+        /// listbox Drop, Add File
+        /// </summary>
+        /// <param name="sender">ListBox</param>
+        /// <param name="e">ListBox.Drop</param>
+        private void AddFile(object sender, DragEventArgs e)
+        {
+            ShowandGetPath(e.Data.GetDataPresent(DataFormats.FileDrop)
+                ? ((string[]) e.Data.GetData(DataFormats.FileDrop)).ToList()
+                : new List<string>());
+        }
+
+        /// <summary>
+        /// Opens a Dialog, and adds the TIFF's on the List
+        /// </summary>
+        /// <param name="p">Extra Item. Can be null</param>
+        public void ShowandGetPath(List<string> p)
+        {
+            var paths = new List<string>();
+            paths.AddRange(p);
+            if (paths.Count == 0)
+            {
+                # region OpenFileDialog ofd
+                OpenFileDialog ofd = new OpenFileDialog()
+                {
+                    AddExtension = false,
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    DefaultExt = "tiff",
+                    Multiselect = true,
+                    Title = "tiff Dosyası seçiniz.",
+                    Filter = "TIF|*.tif;*.TIFF;*tiff;*.TIF;*.TİF;*TİFF"
+                };
+                #endregion
+
+                if (ofd.ShowDialog() == true)
+                {
+                    paths.AddRange(ofd.FileNames.ToList());
+                }
+                else { MessageBox.Show("You have to select something.", "Warning"); return; }
+            }
+            foreach (var item in paths)
+            {
+                var dos = new ConvertFile()
+                {
+                    FileName = item,
+                    Value = 0
+                };
+                FileList.Add(dos);
+                Notify(@"FileList");
+                Notify(@"MiddleText");
+            }
+        }
+        #endregion
+
+        #region Listbox Context
+
+        /// <summary>
+        /// Context> Delete in the list. If Tag is True, then Delete with file
+        /// </summary>
+        /// <param name="sender">MenuItem in the Context</param>
+        /// <param name="e">MenuItem.Click</param>
+        private void Erase(object sender, RoutedEventArgs e)
+        {
+            var sea = new List<ConvertFile>();
+            sea.AddRange(lbConverter.SelectedItems.OfType<ConvertFile>());
+            foreach (var item in sea)
+            {
+                if ((sender as MenuItem)?.Tag.ToString().ToLower() == "true" &&
+                    File.Exists(item.FileName)) File.Delete(item.FileName);
+                FileList.Remove(item);
+            }
+
+        }
+
+        /// <summary>
+        /// Context>Open File
+        /// </summary>
+        /// <param name="sender">MenuItem</param>
+        /// <param name="e">Click</param>
+        private void Follow(object sender, RoutedEventArgs e)
+        {
+            Process.Start((lbConverter.SelectedItem as ConvertFile)?.FileName);
+        }
+
+        /// <summary>
+        /// Context,Add File
+        /// </summary>
+        /// <param name="sender">MenuItem</param>
+        /// <param name="e">Click</param>
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            ShowandGetPath(new List<string>());
+        }
+
+        #endregion
+        
+        /// <summary>
+        /// Settings Save
+        /// </summary>
+        /// <param name="sender">CheckBox</param>
+        /// <param name="e">Click</param>
+        private void SettingsChanged(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+
+        #region Conversion
+
+        /// <summary>
+        /// Works the Worker. How Ironic...
+        /// </summary>
+        /// <param name="sender">Convert Button</param>
+        /// <param name="e"> Click</param>
+        private void ConvertList(object sender, RoutedEventArgs e)
+        {
+            using(BackgroundWorker bw = GetBw())
+            {
+                bw.RunWorkerAsync(FileList);
+            }
+        }
+
+        /// <summary>
+        /// If UseButton is false, convert immediately
+        /// </summary>
+        /// <param name="sender">FileList</param>
+        /// <param name="e">CollectionChanged</param>
+        private void CheckAndIfConvert(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            using(BackgroundWorker bw = GetBw())
+            {
+                if (e.NewItems != null && !Settings.Default.useButton && !bw.IsBusy)
+                    bw.RunWorkerAsync(new List<ConvertFile>(e.NewItems.OfType<ConvertFile>()));
+            }
+            Notify("FileList");
+            Notify("MiddleText");
+            Notify("State");
+        }
+
+        /// <summary>
+        ///a Worker can convert the TIFF Files into PDF's 
+        /// </summary>
+        /// <returns> İşçi</returns>
+        private BackgroundWorker GetBw()
+        {
+            var bw = new BackgroundWorker();
+            bw.DoWork += (s, e) =>
+            {
+                foreach (var item in (List<ConvertFile>) e.Argument)
+                {
+                    if (item.Status != Stat.READY)
+                        continue;
+                    item.Convert();
+                    Application.Current.Dispatcher.Invoke((System.Threading.ThreadStart)delegate
+                    {
+                        ConvertFile.CheckPermissionAndFire(Settings.Default.Erase, () => { FileList.Remove(item); });
+                    });
+                    Notify("FileList");
+                }
+            };
+            return bw;
+        }
+
+        #endregion
+
+        }
+    }
